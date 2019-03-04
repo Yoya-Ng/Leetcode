5eae845113d9ac3e55fd5a3e83de255fc466deee
YoYa <unrealboundary@gmail.com>
2019-03-04 17:38:48 +0800

    JavaWeb

--- src/mydemo/MyDemo.java
+++ src/mydemo/MyDemo.java
@@ -15,7 +15,7 @@ public class MyDemo {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        System.out.println("");
         System.out.println("OK");
     }
     
